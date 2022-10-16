d17b564f06ec04ad04d8be534e15c5c3ead72a31
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-10-16 20:59:39 +0300

    create TaskEditController.swift file for second screen with table view

---
+++ To-Do Manager/TaskEditController.swift
@@ -0,0 +1,12 @@
+//
+//  TaskEditController.swift
+//  To-Do Manager
+//
+//  Created by ALEKSEY SUSLOV on 16.10.2022.
+//
+
+import UIKit
+
+class TaskEditController: UITableViewController {
+    
+}
