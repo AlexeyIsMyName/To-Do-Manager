ea4abec2dfc57f940d3e0c6e295846f487be1b32
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-10-16 21:34:26 +0300

    create TaskTypeController

---
+++ To-Do Manager/TaskTypeController.swift
@@ -0,0 +1,12 @@
+//
+//  TaskTypeController.swift
+//  To-Do Manager
+//
+//  Created by ALEKSEY SUSLOV on 16.10.2022.
+//
+
+import UIKit
+
+class TaskTypeController: UITableViewController {
+    
+}
