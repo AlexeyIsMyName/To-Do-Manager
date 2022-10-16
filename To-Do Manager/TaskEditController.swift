5c8769cf62df1488db291b968fcc1bbae72a8c24
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-10-16 21:09:18 +0300

    make TaskEditController

--- To-Do Manager/TaskEditController.swift
+++ To-Do Manager/TaskEditController.swift
@@ -9,4 +9,19 @@ import UIKit
 
 class TaskEditController: UITableViewController {
     
+    // параметры задачи
+    var taskText: String = ""
+    var taskType: TaskPriority = .normal
+    var taskStatus: TaskStatus = .planned
+    
+    var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void)?
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 }
