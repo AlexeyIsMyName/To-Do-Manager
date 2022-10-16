24dd7ee9ca304ae58871a3ab72012396550f3d71
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-10-16 21:30:04 +0300

    make TaskEditController

--- To-Do Manager/TaskEditController.swift
+++ To-Do Manager/TaskEditController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class TaskEditController: UITableViewController {
     
+    @IBOutlet var taskTitle: UITextField!
+    @IBOutlet var taskTypeLabel: UILabel!
+    
     // параметры задачи
     var taskText: String = ""
     var taskType: TaskPriority = .normal
     var taskStatus: TaskStatus = .planned
     
+    // Название типов задач
+    private var taskTitles: [TaskPriority: String] = [
+        .important: "Важная",
+        .normal: "Текущая"
+    ]
+    
     var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // обновление текстового поля с названием задачи
+        taskTitle?.text = taskText
+        
+        // обновление метки в соответствии текущим типом
+        taskTypeLabel?.text = taskTitles[taskType]
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
